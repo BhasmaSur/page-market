592d778264366589f0f65665b1b85f4cc4b3bebc
mradulmishra <rakshashltd@gmail.com>
2024-01-16 01:41:51 +0530

    v1- structure

--- src/app/api/world/route.js
+++ src/app/api/world/route.js
@@ -2,6 +2,5 @@ import { sql } from "@vercel/postgres";
 
 export async function GET() {
   const { rows } = await sql`SELECT datname FROM pg_database`;
-  console.log(rows);
-  return new Response(rows);
+  return new Response(JSON.stringify(rows));
 }
