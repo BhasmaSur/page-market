807f1f4b9bb7ab1e7bbe749869102c161d31033b
mradulmishra <rakshashltd@gmail.com>
2024-05-15 00:32:30 +0530

    fix

--- material-tailwind.d.ts
+++ material-tailwind.d.ts
@@ -3,19 +3,35 @@ import MaterialTailwind from '@material-tailwind/react'
 declare module '@material-tailwind/react' {
   interface ButtonProps {
     placeholder?
+    onPointerEnterCapture?
+    onPointerLeaveCapture?
   }
 
   interface NavbarProps {
     placeholder?
+    onPointerEnterCapture?
+    onPointerLeaveCapture?
   }
 
-  interface TabsHeader{
+  interface TabsHeaderProps{
     placeholder?
     onPointerEnterCapture?
     onPointerLeaveCapture?
   }
 
   interface TypographyProps {
+    placeholder?
+    onPointerEnterCapture?
+    onPointerLeaveCapture?
+  }
+
+  interface TabProps {
+    placeholder?
+    onPointerEnterCapture?
+    onPointerLeaveCapture?
+  }
+
+  interface CarouselProps {
     placeholder?
     onPointerEnterCapture?
     onPointerLeaveCapture?
